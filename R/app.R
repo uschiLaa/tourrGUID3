250c0fa41df9ae5b4262048fcced18656522989e
Ursula Laa <uschi.laa@gmx.at>
2019-01-15 10:17:23 +1100

    Description and namespace.

--- R/app.R
+++ R/app.R
@@ -7,19 +7,19 @@
 #'
 
 launchApp <- function(inputDataFile){
-  ui <- fluidPage(
-    titlePanel("Welcome to the TourR Shiny app powered by D3.js"),
-    fluidRow(
-      column(3,
-             radioButtons(
+  ui <- shiny::fluidPage(
+    shiny::titlePanel("Welcome to the TourR Shiny app powered by D3.js"),
+    shiny::fluidRow(
+      shiny::column(3,
+             shiny::radioButtons(
                "type",
                label = "Select tour type",
                choices = c("Guided", "Little", "Grand"),
                selected = "Grand"
              ),
-             conditionalPanel(
+             shiny::conditionalPanel(
                "input.type == 'Guided'",
-               selectInput(
+               shiny::selectInput(
                  "guidedIndex",
                  "Index function",
                  c("Holes", "Centre Mass", "LDA", "PDA", "Scagnostics")
@@ -27,9 +27,9 @@ launchApp <- function(inputDataFile){
                  selected = "LDA"
                )
              ),
-             conditionalPanel(
+             shiny::conditionalPanel(
                "input.guidedIndex == 'Scagnostics'",
-               selectInput("scagType", "Scagnostics Metric",
+               shiny::selectInput("scagType", "Scagnostics Metric",
                            choices = list(
                              "Outlying",
                              "Skewed",
@@ -41,14 +41,14 @@ launchApp <- function(inputDataFile){
                              "Stringy",
                              "Monotonic"))),
 
-             fileInput("file1", "Choose CSV File",
+             shiny::fileInput("file1", "Choose CSV File",
                        accept = c(
                          "text/csv",
                          "text/comma-separated-values,text/plain",
                          ".csv")
              ),
 
-             sliderInput(
+             shiny::sliderInput(
                "speed",
                label =  "Tour speed",
                min = 0,
@@ -56,29 +56,28 @@ launchApp <- function(inputDataFile){
                value = 1,
                step = 0.1
              ) ,
-             actionButton("restart_random", "Restart tour with random basis"),
-             selectInput("point_label",choices = vector('character'), label = "Select labeling variable"),
-             selectInput("class", choices = vector('character'), label = "Select class variable to colour the points"),
-             conditionalPanel( condition = "output.numC",
-                               sliderInput("cMax",label = "Threshold value", min=0, max =1, value= 0.5, step = 0.1))
-             # sliderInput("cMax",label = "Threshold value", min="output.minC", max ="output.maxC", value= "output.medC", step = "output.stepC"))
+             shiny::actionButton("restart_random", "Restart tour with random basis"),
+             shiny::selectInput("point_label",choices = vector('character'), label = "Select labeling variable"),
+             shiny::selectInput("class", choices = vector('character'), label = "Select class variable to colour the points"),
+             shiny::conditionalPanel( condition = "output.numC",
+                               shiny::sliderInput("cMax",label = "Threshold value", min=0, max =1, value= 0.5, step = 0.1))
       ),
-      column(3,
-             checkboxGroupInput(
+      shiny::column(3,
+             shiny::checkboxGroupInput(
                "variables",
                label = "Choose variables for the 2D tour",
                choices = vector('character'),
                selected = vector('character')
              )
       ),
-      column(6,
-             tags$script(src = "https://d3js.org/d3.v4.min.js"),
-             tags$script(src="https://d3js.org/d3-scale-chromatic.v1.min.js"),
-             tags$div(id = "d3_output"),
-             tags$div(id = "d3_output_2"),
-             tags$div(id = "info"),
-             tags$div(id = "info_2"),
-             includeScript(system.file("js/d3anim.js", package = "tourrGUID3"))
+      shiny::column(6,
+             shiny::tags$script(src = "https://d3js.org/d3.v4.min.js"),
+             shiny::tags$script(src="https://d3js.org/d3-scale-chromatic.v1.min.js"),
+             shiny::tags$div(id = "d3_output"),
+             shiny::tags$div(id = "d3_output_2"),
+             shiny::tags$div(id = "info"),
+             shiny::tags$div(id = "info_2"),
+             shiny::includeScript(system.file("js/d3anim.js", package = "tourrGUID3"))
              )
     )
   )
@@ -91,53 +90,53 @@ launchApp <- function(inputDataFile){
 
     rv$d <- read.csv(inputDataFile, stringsAsFactors = FALSE)
 
-    observeEvent(input$restart_random,
+    shiny::observeEvent(input$restart_random,
                  {
 
                    p <- length(input$variables)
                    if(p==0){return()}
                    b <- matrix(runif(2*p), p, 2)
 
-                   rv$tour <- new_tour(as.matrix(rv$d[input$variables]),
+                   rv$tour <- tourr::new_tour(as.matrix(rv$d[input$variables]),
                                        choose_tour(input$type, input$guidedIndex, c(rv$class[[1]]), input$scagType),
                                        b)
                  }, ignoreInit = TRUE)
 
-    observeEvent(input$speed, rv$aps <- input$speed)
+    shiny::observeEvent(input$speed, rv$aps <- input$speed)
 
-    observeEvent(input$file1, {
+    shiny::observeEvent(input$file1, {
       inFile <- input$file1
       rv$d <- read.csv(inFile$datapath, stringsAsFactors = FALSE)
     })
 
-    observeEvent(rv$d, {
+    shiny::observeEvent(rv$d, {
 
       rv$nums <- sapply(rv$d, is.numeric)
       rv$groups <- sapply(rv$d, is.character)
 
-      updateCheckboxGroupInput(
+      shiny::updateCheckboxGroupInput(
         session, "variables",
         choices = names(rv$d[rv$nums]),
         selected = names(rv$d[rv$nums])[1:3]
       )
 
-      updateSelectInput(session, "class", choices = names(rv$d),
+      shiny::updateSelectInput(session, "class", choices = names(rv$d),
                         selected = names(rv$d[rv$groups])[1])
-      updateSelectInput(session, "point_label", choices = names(rv$d), selected = names(rv$d)[1])
+      shiny::updateSelectInput(session, "point_label", choices = names(rv$d), selected = names(rv$d)[1])
 
     })
 
 
-    observeEvent(c(input$type, input$variables, input$guidedIndex, input$class, input$scagType, input$point_label, input$cMax),
+    shiny::observeEvent(c(input$type, input$variables, input$guidedIndex, input$class, input$scagType, input$point_label, input$cMax),
                  {
 
                    session$sendCustomMessage("debug", paste("Changed tour type to ", input$type))
                    if (length(input$variables) == 0) {
-                     rv$mat <- rescale(as.matrix(rv$d[names(rv$d[rv$nums])[1:3]]))
+                     rv$mat <- tourr::rescale(as.matrix(rv$d[names(rv$d[rv$nums])[1:3]]))
                      rv$vars <- names(rv$d[rv$nums])[1:3]
                      rv$class <- unname(rv$d[names(rv$d)[1]])
                      if (is.numeric(rv$class[,1])){
-                       output$numC <- reactive(TRUE)
+                       output$numC <- shiny::reactive(TRUE)
                        minC <- min(rv$d[names(rv$d)[1]])
                        maxC <- max(rv$d[names(rv$d)[1]])
                        if((input$cMax >= minC) & (input$cMax <= maxC) ){medC <- input$cMax}
@@ -145,20 +144,20 @@ launchApp <- function(inputDataFile){
                        stepC <- (max(rv$d[names(rv$d)[1]]) - min(rv$d[names(rv$d)[1]])) / 100
                        cl <- rv$class[,1]
                        rv$class <- unname(ifelse(rv$d[names(rv$d)[1]] > input$cMax, "Larger", "Smaller"))
-                       updateSliderInput(session, "cMax", min=minC, max=maxC, value=medC, step=stepC)
+                       shiny::updateSliderInput(session, "cMax", min=minC, max=maxC, value=medC, step=stepC)
                      }
                      else{
                        rv$class <- unname(rv$d[input$class])
-                       output$numC <- reactive(FALSE)
+                       output$numC <- shiny::reactive(FALSE)
                        cl <- rv$class[[1]]
                      }
                      rv$pLabel <- unname(rv$d[names(rv$d)[1]])
                    } else {
 
-                     rv$mat <- rescale(as.matrix(rv$d[input$variables]))
+                     rv$mat <- tourr::rescale(as.matrix(rv$d[input$variables]))
                      rv$vars <- input$variables
                      if (rv$nums[input$class]){
-                       output$numC <- reactive(TRUE)
+                       output$numC <- shiny::reactive(TRUE)
                        minC <- min(rv$d[input$class])
                        maxC <- max(rv$d[input$class])
                        if((input$cMax >= minC) & (input$cMax <= maxC) ){medC <- input$cMax}
@@ -166,26 +165,24 @@ launchApp <- function(inputDataFile){
                        stepC <- (max(rv$d[input$class]) - min(rv$d[input$class])) / 100
                        cl <- rv$class[,1]
                        rv$class <- unname(ifelse(rv$d[input$class] > input$cMax, "Larger", "Smaller"))
-                       updateSliderInput(session, "cMax", min=minC, max=maxC, value=medC, step=stepC)
+                       shiny::updateSliderInput(session, "cMax", min=minC, max=maxC, value=medC, step=stepC)
 
 
                      }
                      else{
                        rv$class <- unname(rv$d[input$class])
-                       output$numC <- reactive(FALSE)
+                       output$numC <- shiny::reactive(FALSE)
                        cl <- rv$class[[1]]
                      }
-                     outputOptions(output, "numC", suspendWhenHidden = FALSE)
+                     shiny::outputOptions(output, "numC", suspendWhenHidden = FALSE)
                      rv$pLabel <- unname(rv$d[input$point_label])
                    }
 
 
-                   #cl <- c(rv$class[[1]])
-
                    session$sendCustomMessage("newcolours", unique(cl))
 
                    rv$tour <-
-                     new_tour(rv$mat,
+                     tourr::new_tour(rv$mat,
                               choose_tour(input$type, input$guidedIndex, cl, input$scagType))
                    rv$ini <- FALSE
                  })
@@ -218,8 +215,8 @@ launchApp <- function(inputDataFile){
         mat_ <- cbind.data.frame(mat, class = cl)
 
 
-        scagResults = c(scagnostics(subset(mat_, class == unique(cl)[1])[1:2])[scagMetricIndex],
-                        scagnostics(subset(mat_, class == unique(cl)[2])[1:2])[scagMetricIndex]
+        scagResults = c(scagnostics::scagnostics(subset(mat_, class == unique(cl)[1])[1:2])[scagMetricIndex],
+                        scagnostics::scagnostics(subset(mat_, class == unique(cl)[2])[1:2])[scagMetricIndex]
 
         )
 
@@ -242,7 +239,7 @@ launchApp <- function(inputDataFile){
       }
     }
 
-    observe({
+    shiny::observe({
 
 
       aps <- rv$aps
@@ -257,14 +254,15 @@ launchApp <- function(inputDataFile){
       }
 
       if (!is.null(step)) {
-        invalidateLater(1000 / fps)
+        shiny::invalidateLater(1000 / fps)
 
-        j <- center(rv$mat %*% step$proj)
+        j <- tourr::center(rv$mat %*% step$proj)
         j <- cbind(j, class = rv$class)
         colnames(j) <- NULL
 
-        session$sendCustomMessage(type = "data", message = list(d = toJSON(data_frame(pL=rv$pLabel[,1],x=j[,2],y=j[,1],c=j[,3])),
-                                                                a = toJSON(data_frame(n=rv$vars,y=step$proj[,1],x=step$proj[,2]))))
+        session$sendCustomMessage(type = "data",
+                                  message = list(d = jsonlite::toJSON(data.frame(pL=rv$pLabel[,1],x=j[,2],y=j[,1],c=j[,3])),
+                                                 a = jsonlite::toJSON(data.frame(n=rv$vars,y=step$proj[,1],x=step$proj[,2]))))
         }
 
       else{
@@ -290,27 +288,25 @@ launchApp <- function(inputDataFile){
 
       if (type == "Grand")
       {
-        tourType <- grand_tour()
+        tourType <- tourr::grand_tour()
       }
       else if (input$type == "Little") {
-        tourType <- little_tour()
+        tourType <- tourr::little_tour()
 
       } else
 
       {
         if (subtype == "Holes") {
-          #browser()
-          tourType <- guided_tour(holes_())
+          tourType <- tourr::guided_tour(holes_())
         } else if (subtype == "Centre Mass") {
-          #browser()
-          tourType <- guided_tour(cmass_())
+          tourType <- tourr::guided_tour(cmass_())
         }
         else if (subtype == "LDA") {
-          tourType <- guided_tour(lda_pp(group_variable))
+          tourType <- tourr::guided_tour(tourr::lda_pp(group_variable))
         } else if (subtype == "PDA") {
-          tourType <- guided_tour(pda_pp(group_variable))
+          tourType <- tourr::guided_tour(tourr::pda_pp(group_variable))
         } else {
-          tourType <- guided_tour(scags(group_variable, scagTypeIndex))
+          tourType <- tourr::guided_tour(scags(group_variable, scagTypeIndex))
         }
 
       }
@@ -319,5 +315,5 @@ launchApp <- function(inputDataFile){
     }
 
   }
-  shinyApp(ui, server)
+  shiny::shinyApp(ui, server)
 }
